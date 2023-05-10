9578bb92a44fddb65e1c5303b76008abda885b3c
Alexandre Borges <ale.tec.hma@gmail.com>
2023-05-10 09:49:16 -0300

    create FormAddTask

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,39 @@
-import { Header } from "../../components/Header";
-import { FormAddTask, HomeContainer, HomeContent } from "./styles";
+import { PlusCircle } from "@phosphor-icons/react";
+
+import { Header } from "../../components/Header/index";
+
+import {
+  FormAddTask,
+  HomeContainer,
+  HomeContent,
+  InfoTask,
+  TasksCompleted,
+  TasksCreated,
+} from "./styles";
 
 export function Home() {
   return (
-    <HomeContainer>
+    <>
       <Header />
       <HomeContent>
         <FormAddTask>
           <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>Criar</button>
+          <button>
+            Criar
+            <PlusCircle size={18} />
+          </button>
         </FormAddTask>
+        <InfoTask>
+          <TasksCreated>
+            <span>Tarefas criadas</span>
+            <span>0</span>
+          </TasksCreated>
+          <TasksCompleted>
+            <span>Concluídas</span>
+            <span>0</span>
+          </TasksCompleted>
+        </InfoTask>
       </HomeContent>
-    </HomeContainer>
+    </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div``;
-
 export const HomeContent = styled.div`
-  width: 736px;
+  width: 46rem;
   height: calc(100vh - 12.5rem);
   display: flex;
   flex-direction: column;
@@ -18,19 +16,63 @@ export const FormAddTask = styled.form`
   width: 100%;
   display: flex;
   gap: 0.5rem;
+  padding-top: -10px;
+  margin-top: -1rem;
+
   input {
     width: 100%;
     height: 54px;
     background: ${(props) => props.theme["gray-500"]};
     border: 1px solid ${(props) => props.theme["gray-700"]};
+    color: ${(props) => props.theme["gray-100"]};
     border-radius: 8px;
+    border: 0;
+    padding: 1rem;
   }
   button {
-    width: 90px;
-    height: 52px;
-    background: ${(props) => props.theme["blue-500"]};
+    display: flex;
+    align-items: center;
+    padding: 1em;
+    gap: 0.25em;
+    border: none;
     border-radius: 8px;
+    background: ${(props) => props.theme["blue-500"]};
     color: ${(props) => props.theme["gray-100"]};
   }
+  svg {
+    margin-bottom: -2px;
+  }
 `;
-//
+
+export const InfoTask = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 4rem;
+  font-weight: 700;
+  line-height: 17px;
+`;
+
+export const TasksCreated = styled.div`
+  color: ${(props) => props.theme["blue-300"]};
+
+  span:last-child {
+    background: ${(props) => props.theme["gray-400"]};
+    color: ${(props) => props.theme["gray-200"]};
+    padding: 2px 8px;
+    border-radius: 999px;
+    margin-left: 0.5rem;
+  }
+`;
+
+export const TasksCompleted = styled.div`
+  color: ${(props) => props.theme["purple-300"]};
+
+  span:last-child {
+    background: ${(props) => props.theme["gray-400"]};
+    color: ${(props) => props.theme["gray-200"]};
+    padding: 2px 8px;
+    border-radius: 999px;
+    margin-left: 0.5rem;
+  }
+`;
